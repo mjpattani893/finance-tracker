803e0b66a82282c4821789e1ffd000ae7ffb3b47
Maitreyi <maitreyipattani@gmail.com>
2017-05-09 09:51:31 -0700

    View friends profile and stocks

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :friendships
+
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 end
